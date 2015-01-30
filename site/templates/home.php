b6e2a7ce4bd6bb81a8d7f5a24fce915a286d14f1
janine.derix <janine.derix@pixolith.de>
2015-01-30 12:21:06 +0100

    + new stuff - old stuff

--- site/templates/home.php
+++ site/templates/home.php
@@ -3,7 +3,7 @@
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Css-Akkordeon</title>
-  <link rel="stylesheet" href="stylesheets/screen.css" />
+  <link rel="stylesheet" href="/assets/css/styles.min.css" />
 </head>
 
 <body>
