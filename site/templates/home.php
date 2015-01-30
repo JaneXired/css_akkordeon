3c3af5c4328fdc1d03648e066d7e806ca26ce7e7
Simon Kunz <simon.kunz@pixolith.de>
2015-01-30 14:15:58 +0100

    changed structure of html to php partials

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,66 +1,7 @@
-<!doctype html>
-<head>
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Css-Akkordeon</title>
-  <link rel="stylesheet" href="/assets/css/styles.min.css" />
-</head>
-
-<body>
-
-<div class="wrapper">
-
-  <!-- START OF ACCORDION-->
-  <dl>
-    <dt>
-    <h1><a href="#accordion1">Überschrift 1</a></h1>
-    </dt>
-    <dd id="accordion1">
-      <hr>
-      <p>
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
-        et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
-        Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
-        consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-        sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
-        no sea takimata sanctus est Lorem ipsum dolor sit amet.
-      </p>
-    </dd>
-
-    <dt>
-    <h1><a href="#accordion2">Überschrift 1</a></h1>
-    </dt>
-    <dd id="accordion2">
-      <hr>
-      <p>
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
-        et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
-        Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
-        consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-        sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
-        no sea takimata sanctus est Lorem ipsum dolor sit amet.
-      </p>
-    </dd>
-
-    <dt>
-    <h1><a href="#accordion3">Überschrift 1</a></h1>
-    </dt>
-    <dd id="accordion3">
-      <hr>
-      <p>
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
-        et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
-        Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
-        consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-        sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
-        no sea takimata sanctus est Lorem ipsum dolor sit amet.
-      </p>
-    </dd>
-  </dl>
-  <!--END OF ACCORDION-->
-</div>
-
-</body>
-
+<?php snippet('header') ?>
+<main role="content">
+    <div class="wrapper">
+        <?php snippet('accordion') ?>
+    </div>
 </main>
-
+<?php snippet('footer') ?>
