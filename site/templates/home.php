b4a27ce5d9791df4924fcc2122091f5ad52aa9cd
Simon Kunz <pattexman@web.de>
2015-03-12 16:19:13 +0100

    started working on an accordion

--- site/templates/home.php
+++ site/templates/home.php
@@ -4,6 +4,7 @@
         <?php snippet('accordion') ?>
         <?php snippet('zoom') ?>
         <?php snippet('svg-menu') ?>
+        <?php snippet('accordion_2') ?>
     </div>
 </main>
 <?php snippet('footer') ?>
