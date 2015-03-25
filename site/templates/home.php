8c342163bbee2a847219f3655b26ddd9bd2d5731
Simon Kunz <pattexman@web.de>
2015-03-25 18:14:21 +0100

    fixed chrome

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,9 +1,10 @@
 <?php snippet('header') ?>
-<?php snippet('svg-menu') ?>
+
 <main role="content">
     <div class="parallax">
         <div class="parallax__group">
             <div class="wrapper">
+                <?php snippet('svg-menu') ?>
                 <section class="intro">This page aims to show 60fps CSS-only implementations focusing on performance and new techniques rather than best practices / browser compatibility.</section>
             </div>
         </div>
