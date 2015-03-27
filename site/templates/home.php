a8bcc1c59539722c0449c4754e18f6524809f7a1
Simon Kunz <pattexman@web.de>
2015-03-27 01:24:30 +0100

    yoda

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,11 +1,42 @@
 <?php snippet('header') ?>
 
 <main role="content">
+    <?php
+    for ($x = 0; $x <= 10; $x++) {
+        echo '<div class="square"></div>';
+    }
+    ?>
+
     <div class="parallax">
         <div class="parallax__group">
             <div class="wrapper">
                 <?php snippet('svg-menu') ?>
-                <section class="intro">This page aims to show 60fps CSS-only implementations focusing on performance and new techniques rather than best practices / browser compatibility.</section>
+                <section class="intro">
+                    <div>
+                        <svg viewBox="0 0 375 80">
+                            <defs>
+                                <filter id="shadow">
+                                    <feGaussianBlur in='SourceAlpha' stdDeviation='2.5' result='blur' />
+                                    <feColorMatrix type="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 .35 0" result="blur"/>
+                                    <feMerge>
+                                        <feMergeNode in='blur'/>
+                                        <feMergeNode in='SourceGraphic'/>
+                                    </feMerge>
+                                </filter>
+                            </defs>
+                            <g>
+                                <g fill="#f2f2f2" filter="url(#shadow)">
+                                    <rect width="335" height="80" rx="5" ry="5"/>
+                                    <path d="M -20 20 l 21 -10 0 20 z" transform="translate(335,0) scale(-1,1)"/>
+                                </g>
+                                <text y="65%">
+                                    <tspan x="15%">CSS you must.</tspan>
+                                </text>
+                            </g>
+                        </svg>
+                        <?php snippet('yoda') ?>
+                    </div>
+                </section>
             </div>
         </div>
         <div class="parallax__group">
